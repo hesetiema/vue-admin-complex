9d3c5f989d7b06c979fb4d226671eafc7b689adf
wuyuxi <980025512@qq.com>
2024-04-30 00:22:15 +0800

    feat: 修改部署地址

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/vue-admin-complex/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
